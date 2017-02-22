3fc73ef955e307684200cc4c46c7efe616e9a767
mfaiza <mohamed-iheb.faiza@etu.umontpellier.fr>
2017-02-22 23:36:31 +0100

    fixed audio bug

--- security_main.c
+++ security_main.c
@@ -3,7 +3,7 @@
 #include "library/grovepi.h"
 
 //Connexion : ssh -Y faizabenchraa@162.38.111.124
-//Pwd : faizabenchraa 
+//Pwd : faizabenchraa
 //Compilation : gcc security_main.c library/grovepi.c -o security_main
 //Execution : sudo ./security_main
 //Affichage : xdg-open photo.jpg
@@ -22,7 +22,7 @@ int main(void)
 
 	//Augmenter le son au maximum
 	system("amixer -D pulse sset Master 100%") ;
-	
+
 	while(1) // boucle infinie
 	{
 	  write_block(us_cmd, PIN, 0, 0);
@@ -33,11 +33,11 @@ int main(void)
 	  printf("%d %d\n", r_buf[1], r_buf[2]);
 	  printf("Distance : %d cm\n", data);
 		if ((data>100.0) && (data<=150.0)){
-			system("omxplayer resources/alarme_boosted.wav") ; //lit le message audio
+			system("omxplayer -o local resources/alarme_boosted.wav") ; //lit le message audio, -o local force la sortie sur jack
 		}
 		else if (data<=100.0){
 			// -t : temps de capture (ms). -v : pour afficher les détails. -vf -hf : inverser l'image.
-			//Pour les videos : raspivid -t 5000 -o video.h264 
+			//Pour les videos : raspivid -t 5000 -o video.h264
 			system("raspistill -t 500 -v -vf -hf -o photos/$(date +'%Y-%m-%d_%H%M').jpg") ;
 			//appel script python pour l'envoi
 			system("python resources/envoyer.py photos/$(date +'%Y-%m-%d_%H%M').jpg") ;
